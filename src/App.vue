56d46dfffd8a193f153f608ef2debca0ea38f970
Natalia <glukomani@gmail.com>
2024-05-31 11:24:31 +0200

    First start

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>
+    {{info}}
+  </h1>
+  <p>Some text</p>
+
+  <button type="button" @click="this.info = 'Clicked'">Send</button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        info: 'Title'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+p {
+  color: green;
+}
+</style>
